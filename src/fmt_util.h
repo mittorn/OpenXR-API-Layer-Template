71b0edb37e1f0810bb53630fd36e803adfbe5084
mittorn <mittorn@sibmail.com>
2024-03-04 07:57:19 +0300

    Rewrite bloating point code in more compact way, allow loop unrolling and do less branching

--- src/fmt_util.h
+++ src/fmt_util.h
@@ -29,150 +29,15 @@ so enabling compiler optimization is very recommended
 #endif // __GNUC__
 
 // stb_sprintf float utility
+#define STBSP__SPECIAL 0x7000
 
 #define stbsp__uint32 unsigned int
 #define stbsp__int32 signed int
-
-#ifdef _MSC_VER
-#define stbsp__uint64 unsigned __int64
-#define stbsp__int64 signed __int64
-#else
 #define stbsp__uint64 unsigned long long
 #define stbsp__int64 signed long long
-#endif
 #define stbsp__uint16 unsigned short
 
-#define STBSP__SPECIAL 0x7000
-constexpr static inline struct
-{
-   short temp; // force next field to be 2-byte aligned
-   char pair[201];
-} stbsp__digitpair =
-{
-  0,
-   "00010203040506070809101112131415161718192021222324"
-   "25262728293031323334353637383940414243444546474849"
-   "50515253545556575859606162636465666768697071727374"
-   "75767778798081828384858687888990919293949596979899"
-};
-
-// copies d to bits w/ strict aliasing (this compiles to nothing on /Ox)
-#define STBSP__COPYFP(dest, src)                   \
-   {                                               \
-	  int cn;                                      \
-	  for (cn = 0; cn < 8; cn++)                   \
-		 ((char *)&dest)[cn] = ((char *)&src)[cn]; \
-   }
-
-// get float info
-static stbsp__int32 stbsp__real_to_parts(stbsp__int64 *bits, stbsp__int32 *expo, double value)
-{
-   double d;
-   stbsp__int64 b = 0;
-
-   // load value and round at the frac_digits
-   d = value;
-
-   STBSP__COPYFP(b, d);
-
-   *bits = b & ((((stbsp__uint64)1) << 52) - 1);
-   *expo = (stbsp__int32)(((b >> 52) & 2047) - 1023);
-
-   return (stbsp__int32)((stbsp__uint64) b >> 63);
-}
-
-static double const stbsp__bot[23] = {
-   1e+000, 1e+001, 1e+002, 1e+003, 1e+004, 1e+005, 1e+006, 1e+007, 1e+008, 1e+009, 1e+010, 1e+011,
-   1e+012, 1e+013, 1e+014, 1e+015, 1e+016, 1e+017, 1e+018, 1e+019, 1e+020, 1e+021, 1e+022
-};
-static double const stbsp__negbot[22] = {
-   1e-001, 1e-002, 1e-003, 1e-004, 1e-005, 1e-006, 1e-007, 1e-008, 1e-009, 1e-010, 1e-011,
-   1e-012, 1e-013, 1e-014, 1e-015, 1e-016, 1e-017, 1e-018, 1e-019, 1e-020, 1e-021, 1e-022
-};
-static double const stbsp__negboterr[22] = {
-   -5.551115123125783e-018,  -2.0816681711721684e-019, -2.0816681711721686e-020, -4.7921736023859299e-021, -8.1803053914031305e-022, 4.5251888174113741e-023,
-   4.5251888174113739e-024,  -2.0922560830128471e-025, -6.2281591457779853e-026, -3.6432197315497743e-027, 6.0503030718060191e-028,  2.0113352370744385e-029,
-   -3.0373745563400371e-030, 1.1806906454401013e-032,  -7.7705399876661076e-032, 2.0902213275965398e-033,  -7.1542424054621921e-034, -7.1542424054621926e-035,
-   2.4754073164739869e-036,  5.4846728545790429e-037,  9.2462547772103625e-038,  -4.8596774326570872e-039
-};
-static double const stbsp__top[13] = {
-   1e+023, 1e+046, 1e+069, 1e+092, 1e+115, 1e+138, 1e+161, 1e+184, 1e+207, 1e+230, 1e+253, 1e+276, 1e+299
-};
-static double const stbsp__negtop[13] = {
-   1e-023, 1e-046, 1e-069, 1e-092, 1e-115, 1e-138, 1e-161, 1e-184, 1e-207, 1e-230, 1e-253, 1e-276, 1e-299
-};
-static double const stbsp__toperr[13] = {
-   8388608,
-   6.8601809640529717e+028,
-   -7.253143638152921e+052,
-   -4.3377296974619174e+075,
-   -1.5559416129466825e+098,
-   -3.2841562489204913e+121,
-   -3.7745893248228135e+144,
-   -1.7356668416969134e+167,
-   -3.8893577551088374e+190,
-   -9.9566444326005119e+213,
-   6.3641293062232429e+236,
-   -5.2069140800249813e+259,
-   -5.2504760255204387e+282
-};
-static double const stbsp__negtoperr[13] = {
-   3.9565301985100693e-040,  -2.299904345391321e-063,  3.6506201437945798e-086,  1.1875228833981544e-109,
-   -5.0644902316928607e-132, -6.7156837247865426e-155, -2.812077463003139e-178,  -5.7778912386589953e-201,
-   7.4997100559334532e-224,  -4.6439668915134491e-247, -6.3691100762962136e-270, -9.436808465446358e-293,
-   8.0970921678014997e-317
-};
-
-#if defined(_MSC_VER) && (_MSC_VER <= 1200)
-static stbsp__uint64 const stbsp__powten[20] = {
-   1,
-   10,
-   100,
-   1000,
-   10000,
-   100000,
-   1000000,
-   10000000,
-   100000000,
-   1000000000,
-   10000000000,
-   100000000000,
-   1000000000000,
-   10000000000000,
-   100000000000000,
-   1000000000000000,
-   10000000000000000,
-   100000000000000000,
-   1000000000000000000,
-   10000000000000000000U
-};
-#define stbsp__tento19th ((stbsp__uint64)1000000000000000000)
-#else
-constexpr static stbsp__uint64 const stbsp__powten[20] = {
-   1,
-   10,
-   100,
-   1000,
-   10000,
-   100000,
-   1000000,
-   10000000,
-   100000000,
-   1000000000,
-   10000000000ULL,
-   100000000000ULL,
-   1000000000000ULL,
-   10000000000000ULL,
-   100000000000000ULL,
-   1000000000000000ULL,
-   10000000000000000ULL,
-   100000000000000000ULL,
-   1000000000000000000ULL,
-   10000000000000000000ULL
-};
-#define stbsp__tento19th (1000000000000000000ULL)
-#endif
-
+#define STBSP__COPYFP(dest, src)  *(stbsp__uint64*)&dest = *(stbsp__uint64*)&src
 #define stbsp__ddmulthi(oh, ol, xh, yh)                            \
    {                                                               \
 	  double ahi = 0, alo, bhi = 0, blo;                           \
@@ -188,7 +53,6 @@ constexpr static stbsp__uint64 const stbsp__powten[20] = {
 	  blo = yh - bhi;                                              \
 	  ol = ((ahi * bhi - oh) + ahi * blo + alo * bhi) + alo * blo; \
    }
-
 #define stbsp__ddtoS64(ob, xh, xl)          \
    {                                        \
 	  double ahi = 0, alo, vh, t;           \
@@ -199,7 +63,6 @@ constexpr static stbsp__uint64 const stbsp__powten[20] = {
 	  alo = (xh - (ahi - t)) - (vh + t);    \
 	  ob += (stbsp__int64)(ahi + alo + xl); \
    }
-
 #define stbsp__ddrenorm(oh, ol) \
    {                            \
 	  double s;                 \
@@ -207,230 +70,245 @@ constexpr static stbsp__uint64 const stbsp__powten[20] = {
 	  ol = ol - (s - oh);       \
 	  oh = s;                   \
    }
-
 #define stbsp__ddmultlo(oh, ol, xh, xl, yh, yl) ol = ol + (xh * yl + xl * yh);
-
 #define stbsp__ddmultlos(oh, ol, xh, yl) ol = ol + (xh * yl);
 
 forceinline static inline void stbsp__raise_to_power10(double *ohi, double *olo, double d, stbsp__int32 power) // power can be -323 to +350
 {
-   double ph, pl;
-   if ((power >= 0) && (power <= 22)) {
-	  stbsp__ddmulthi(ph, pl, d, stbsp__bot[power]);
-   } else {
-	  stbsp__int32 e, et, eb;
-	  double p2h, p2l;
+	constexpr static double const stbsp__bot[23] = {
+		1e+000, 1e+001, 1e+002, 1e+003, 1e+004, 1e+005, 1e+006, 1e+007, 1e+008, 1e+009, 1e+010, 1e+011,
+		1e+012, 1e+013, 1e+014, 1e+015, 1e+016, 1e+017, 1e+018, 1e+019, 1e+020, 1e+021, 1e+022
+	};
+	constexpr static double const stbsp__negbot[22] = {
+		1e-001, 1e-002, 1e-003, 1e-004, 1e-005, 1e-006, 1e-007, 1e-008, 1e-009, 1e-010, 1e-011,
+		1e-012, 1e-013, 1e-014, 1e-015, 1e-016, 1e-017, 1e-018, 1e-019, 1e-020, 1e-021, 1e-022
+	};
+	constexpr static double const stbsp__negboterr[22] = {
+		-5.551115123125783e-018,  -2.0816681711721684e-019, -2.0816681711721686e-020, -4.7921736023859299e-021, -8.1803053914031305e-022, 4.5251888174113741e-023,
+		4.5251888174113739e-024,  -2.0922560830128471e-025, -6.2281591457779853e-026, -3.6432197315497743e-027, 6.0503030718060191e-028,  2.0113352370744385e-029,
+		-3.0373745563400371e-030, 1.1806906454401013e-032,  -7.7705399876661076e-032, 2.0902213275965398e-033,  -7.1542424054621921e-034, -7.1542424054621926e-035,
+		2.4754073164739869e-036,  5.4846728545790429e-037,  9.2462547772103625e-038,  -4.8596774326570872e-039
+	};
+	constexpr static double const stbsp__top[13] = {
+		1e+023, 1e+046, 1e+069, 1e+092, 1e+115, 1e+138, 1e+161, 1e+184, 1e+207, 1e+230, 1e+253, 1e+276, 1e+299
+	};
+	constexpr static double const stbsp__negtop[13] = {
+		1e-023, 1e-046, 1e-069, 1e-092, 1e-115, 1e-138, 1e-161, 1e-184, 1e-207, 1e-230, 1e-253, 1e-276, 1e-299
+	};
+	constexpr static double const stbsp__toperr[13] = {
+		8388608,
+		6.8601809640529717e+028,
+		-7.253143638152921e+052,
+		-4.3377296974619174e+075,
+		-1.5559416129466825e+098,
+		-3.2841562489204913e+121,
+		-3.7745893248228135e+144,
+		-1.7356668416969134e+167,
+		-3.8893577551088374e+190,
+		-9.9566444326005119e+213,
+		6.3641293062232429e+236,
+		-5.2069140800249813e+259,
+		-5.2504760255204387e+282
+	};
+	constexpr static double const stbsp__negtoperr[13] = {
+		3.9565301985100693e-040,  -2.299904345391321e-063,  3.6506201437945798e-086,  1.1875228833981544e-109,
+		-5.0644902316928607e-132, -6.7156837247865426e-155, -2.812077463003139e-178,  -5.7778912386589953e-201,
+		7.4997100559334532e-224,  -4.6439668915134491e-247, -6.3691100762962136e-270, -9.436808465446358e-293,
+		8.0970921678014997e-317
+	};
+	double ph, pl;
+	if ((power >= 0) && (power <= 22)) {
+		stbsp__ddmulthi(ph, pl, d, stbsp__bot[power]);
+	} else {
+		stbsp__int32 e, et, eb;
+		double p2h, p2l;
 
-	  e = power;
-	  if (power < 0)
-		 e = -e;
-	  et = (e * 0x2c9) >> 14; /* %23 */
-	  if (et > 13)
-		 et = 13;
-	  eb = e - (et * 23);
+		e = power;
+		if (power < 0)
+			e = -e;
+		et = (e * 0x2c9) >> 14; /* %23 */
+		if (et > 13)
+			et = 13;
+		eb = e - (et * 23);
 
-	  ph = d;
-	  pl = 0.0;
-	  if (power < 0) {
-		 if (eb) {
-			--eb;
-			stbsp__ddmulthi(ph, pl, d, stbsp__negbot[eb]);
-			stbsp__ddmultlos(ph, pl, d, stbsp__negboterr[eb]);
-		 }
-		 if (et) {
-			stbsp__ddrenorm(ph, pl);
-			--et;
-			stbsp__ddmulthi(p2h, p2l, ph, stbsp__negtop[et]);
-			stbsp__ddmultlo(p2h, p2l, ph, pl, stbsp__negtop[et], stbsp__negtoperr[et]);
-			ph = p2h;
-			pl = p2l;
-		 }
-	  } else {
-		 if (eb) {
-			e = eb;
-			if (eb > 22)
-			   eb = 22;
-			e -= eb;
-			stbsp__ddmulthi(ph, pl, d, stbsp__bot[eb]);
-			if (e) {
-			   stbsp__ddrenorm(ph, pl);
-			   stbsp__ddmulthi(p2h, p2l, ph, stbsp__bot[e]);
-			   stbsp__ddmultlos(p2h, p2l, stbsp__bot[e], pl);
-			   ph = p2h;
-			   pl = p2l;
+		ph = d;
+		pl = 0.0;
+		if (power < 0) {
+			if (eb) {
+				--eb;
+				stbsp__ddmulthi(ph, pl, d, stbsp__negbot[eb]);
+				stbsp__ddmultlos(ph, pl, d, stbsp__negboterr[eb]);
 			}
-		 }
-		 if (et) {
-			stbsp__ddrenorm(ph, pl);
-			--et;
-			stbsp__ddmulthi(p2h, p2l, ph, stbsp__top[et]);
-			stbsp__ddmultlo(p2h, p2l, ph, pl, stbsp__top[et], stbsp__toperr[et]);
-			ph = p2h;
-			pl = p2l;
-		 }
-	  }
-   }
-   stbsp__ddrenorm(ph, pl);
-   *ohi = ph;
-   *olo = pl;
-}
-
-// given a float value, returns the significant bits in bits, and the position of the
-//   decimal point in decimal_pos.  +/-INF and NAN are specified by special values
-//   returned in the decimal_pos parameter.
-// frac_digits is absolute normally, but if you want from first significant digits (got %g and %e), or in 0x80000000
-static inline stbsp__int32 stbsp__real_to_str(char const **start, stbsp__uint32 *len, char *out, stbsp__int32 *decimal_pos, double value, stbsp__uint32 frac_digits)
-{
-   double d;
-   stbsp__int64 bits = 0;
-   stbsp__int32 expo, e, ng, tens;
-
-   d = value;
-   STBSP__COPYFP(bits, d);
-   expo = (stbsp__int32)((bits >> 52) & 2047);
-   ng = (stbsp__int32)((stbsp__uint64) bits >> 63);
-   if (ng)
-	  d = -d;
-
-   if (expo == 2047) // is nan or inf?
-   {
-	  *start = (bits & ((((stbsp__uint64)1) << 52) - 1)) ? "NaN" : "Inf";
-	  *decimal_pos = STBSP__SPECIAL;
-	  *len = 3;
-	  return ng;
-   }
-
-   if (expo == 0) // is zero or denormal
-   {
-	  if (((stbsp__uint64) bits << 1) == 0) // do zero
-	  {
-		 *decimal_pos = 1;
-		 *start = out;
-		 out[0] = '0';
-		 *len = 1;
-		 return ng;
-	  }
-	  // find the right expo for denormals
-	  {
-		 stbsp__int64 v = ((stbsp__uint64)1) << 51;
-		 while ((bits & v) == 0) {
-			--expo;
-			v >>= 1;
-		 }
-	  }
-   }
-
-   // find the decimal exponent as well as the decimal bits of the value
-   {
-	  double ph, pl;
-
-	  // log10 estimate - very specifically tweaked to hit or undershoot by no more than 1 of log10 of all expos 1..2046
-	  tens = expo - 1023;
-	  tens = (tens < 0) ? ((tens * 617) / 2048) : (((tens * 1233) / 4096) + 1);
-
-	  // move the significant bits into position and stick them into an int
-	  stbsp__raise_to_power10(&ph, &pl, d, 18 - tens);
-
-	  // get full as much precision from double-double as possible
-	  stbsp__ddtoS64(bits, ph, pl);
-
-	  // check if we undershot
-	  if (((stbsp__uint64)bits) >= stbsp__tento19th)
-		 ++tens;
-   }
-
-   // now do the rounding in integer land
-   frac_digits = (frac_digits & 0x80000000) ? ((frac_digits & 0x7ffffff) + 1) : (tens + frac_digits);
-   if ((frac_digits < 24)) {
-	  stbsp__uint32 dg = 1;
-	  if ((stbsp__uint64)bits >= stbsp__powten[9])
-		 dg = 10;
-	  while ((stbsp__uint64)bits >= stbsp__powten[dg]) {
-		 ++dg;
-		 if (dg == 20)
-			goto noround;
-	  }
-	  if (frac_digits < dg) {
-		 stbsp__uint64 r;
-		 // add 0.5 at the right position and round
-		 e = dg - frac_digits;
-		 if ((stbsp__uint32)e >= 24)
-			goto noround;
-		 r = stbsp__powten[e];
-		 bits = bits + (r / 2);
-		 if ((stbsp__uint64)bits >= stbsp__powten[dg])
-			++tens;
-		 bits /= r;
-	  }
-   noround:;
-   }
-
-   // kill long trailing runs of zeros
-   if (bits) {
-	  stbsp__uint32 n;
-	  for (;;) {
-		 if (bits <= 0xffffffff)
-			break;
-		 if (bits % 1000)
-			goto donez;
-		 bits /= 1000;
-	  }
-	  n = (stbsp__uint32)bits;
-	  while ((n % 1000) == 0)
-		 n /= 1000;
-	  bits = n;
-   donez:;
-   }
-
-   // convert to string
-   out += 64;
-   e = 0;
-   for (;;) {
-	  stbsp__uint32 n;
-	  char *o = out - 8;
-	  // do the conversion in chunks of U32s (avoid most 64-bit divides, worth it, constant denomiators be damned)
-	  if (bits >= 100000000) {
-		 n = (stbsp__uint32)(bits % 100000000);
-		 bits /= 100000000;
-	  } else {
-		 n = (stbsp__uint32)bits;
-		 bits = 0;
-	  }
-	  while (n) {
-		 out -= 2;
-		 *(stbsp__uint16 *)out = *(stbsp__uint16 *)&stbsp__digitpair.pair[(n % 100) * 2];
-		 n /= 100;
-		 e += 2;
-	  }
-	  if (bits == 0) {
-		 if ((e) && (out[0] == '0')) {
-			++out;
-			--e;
-		 }
-		 break;
-	  }
-	  while (out != o) {
-		 *--out = '0';
-		 ++e;
-	  }
-   }
-
-   *decimal_pos = tens;
-   *start = out;
-   *len = e;
-   return ng;
+			if (et) {
+				stbsp__ddrenorm(ph, pl);
+				--et;
+				stbsp__ddmulthi(p2h, p2l, ph, stbsp__negtop[et]);
+				stbsp__ddmultlo(p2h, p2l, ph, pl, stbsp__negtop[et], stbsp__negtoperr[et]);
+				ph = p2h;
+				pl = p2l;
+			}
+		} else {
+			if (eb) {
+				e = eb;
+				if (eb > 22)
+					eb = 22;
+				e -= eb;
+				stbsp__ddmulthi(ph, pl, d, stbsp__bot[eb]);
+				if (e) {
+					stbsp__ddrenorm(ph, pl);
+					stbsp__ddmulthi(p2h, p2l, ph, stbsp__bot[e]);
+					stbsp__ddmultlos(p2h, p2l, stbsp__bot[e], pl);
+					ph = p2h;
+					pl = p2l;
+				}
+			}
+			if (et) {
+				stbsp__ddrenorm(ph, pl);
+				--et;
+				stbsp__ddmulthi(p2h, p2l, ph, stbsp__top[et]);
+				stbsp__ddmultlo(p2h, p2l, ph, pl, stbsp__top[et], stbsp__toperr[et]);
+				ph = p2h;
+				pl = p2l;
+			}
+		}
+	}
+	stbsp__ddrenorm(ph, pl);
+	*ohi = ph;
+	*olo = pl;
 }
 
 #undef stbsp__ddmulthi
 #undef stbsp__ddrenorm
 #undef stbsp__ddmultlo
 #undef stbsp__ddmultlos
-// used
-//#undef STBSP__SPECIAL
+
+// given a float value, returns the significant bits in bits, and the position of the
+//   decimal point in decimal_pos.  +/-INF and NAN are specified by special values
+//   returned in the decimal_pos parameter.
+// frac_digits is absolute normally, but if you want from first significant digits (got %g and %e), or in 0x80000000
+static inline stbsp__int32 stbsp__real_to_str(char const **start, stbsp__uint32 *len, char *out, stbsp__int32 *decimal_pos, double d, stbsp__uint32 frac_digits)
+{
+	constexpr static stbsp__uint64 const stbsp__powten[20] = {
+		1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000,
+		1000000000, 10000000000ULL, 100000000000ULL,
+		1000000000000ULL, 10000000000000ULL, 100000000000000ULL,
+		1000000000000000ULL, 10000000000000000ULL, 100000000000000000ULL,
+		1000000000000000000ULL, 10000000000000000000ULL
+	};
+	constexpr static stbsp__uint64 const stbsp__tento19th = (1000000000000000000ULL);
+	stbsp__int64 bits = 0;
+	stbsp__int32 expo, e, ng, tens;
+
+	STBSP__COPYFP(bits, d);
+	expo = (stbsp__int32)((bits >> 52) & 2047);
+	ng = (stbsp__int32)((stbsp__uint64) bits >> 63);
+	if (ng)
+		d = -d;
+
+	if (expo == 2047) // is nan or inf?
+	{
+		*start = (bits & ((((stbsp__uint64)1) << 52) - 1)) ? "NaN" : "Inf";
+		*decimal_pos = STBSP__SPECIAL;
+		*len = 3;
+		return ng;
+	}
+
+	if (expo == 0) // is zero or denormal
+	{
+		if (((stbsp__uint64) bits << 1) == 0) // do zero
+		{
+			*decimal_pos = 1;
+			*start = out;
+			out[0] = '0';
+			*len = 1;
+			return ng;
+		}
+		// find the right expo for denormals
+		{
+			stbsp__int64 v = ((stbsp__uint64)1) << 51;
+			while ((bits & v) == 0) {
+				--expo;
+				v >>= 1;
+			}
+		}
+	}
+
+	// find the decimal exponent as well as the decimal bits of the value
+	{
+		double ph, pl;
+
+		// log10 estimate - very specifically tweaked to hit or undershoot by no more than 1 of log10 of all expos 1..2046
+		tens = expo - 1023;
+		tens = (tens < 0) ? ((tens * 617) / 2048) : (((tens * 1233) / 4096) + 1);
+
+		// move the significant bits into position and stick them into an int
+		stbsp__raise_to_power10(&ph, &pl, d, 18 - tens);
+
+		// get full as much precision from double-double as possible
+		stbsp__ddtoS64(bits, ph, pl);
+
+		// check if we undershot
+		if (((stbsp__uint64)bits) >= stbsp__tento19th)
+			++tens;
+	}
+
+	// now do the rounding in integer land
+	frac_digits = (frac_digits & 0x80000000) ? ((frac_digits & 0x7ffffff) + 1) : (tens + frac_digits);
+	if ((frac_digits < 24)) {
+		stbsp__uint32 dg = 1;
+		if ((stbsp__uint64)bits >= stbsp__powten[9])
+			dg = 10;
+		#pragma GCC unroll 128
+		for( int i = dg; i < 20; i++)
+			dg += (stbsp__uint64)bits >= stbsp__powten[i];
+
+		if (frac_digits < dg || dg == 20) {
+			stbsp__uint64 r;
+			// add 0.5 at the right position and round
+			e = dg - frac_digits;
+			if ((stbsp__uint32)e < 24)
+			{
+				r = stbsp__powten[e];
+				bits = bits + (r / 2);
+				if ((stbsp__uint64)bits >= stbsp__powten[dg])
+					++tens;
+				bits /= r;
+			}
+		}
+	}
+
+	// kill long trailing runs of zeros
+	#pragma GCC unroll 128
+	for(int i = 0; i < 6; i++)
+	{
+			stbsp__uint64 n = !(bits % 1000) - 1;
+			bits = (~n & (bits / 1000)) | (n & bits);
+	}
+
+	// convert to string
+	out += 64;
+	e = 0;
+	stbsp__uint64 n = bits;
+#pragma GCC unroll 128
+	for(int i = 0; i < 19; i++)
+	{
+		int o = n > 0;
+		*out = (n % 10) + '0';
+		out -= o, e+= o;
+		n /= 10;
+	}
+	if ((e) && (out[0] == '0'))
+		out++,e--;
+	e++;
+	*decimal_pos = tens;
+	*start = out;
+	*len = e;
+	return ng;
+}
+#undef stbsp__ddtoS64
 #undef STBSP__COPYFP
 
 // Type traits
-
 
 template<class A, class B>
 struct CompareTypes_w {
@@ -445,11 +323,7 @@ struct CompareTypes_w<T, T>{
 // if constexpr(CompareTypes(T,int)) { ... }
 #define CompareTypes(A,B) (CompareTypes_w<A,B>::res)
 
-
-
 // OverloadSet utility
-
-
 template<class... Fs> struct FunctionOverloadSet{};
 
 template<class F0, class...Fs>
@@ -470,7 +344,6 @@ template<class...Fs> constexpr FunctionOverloadSet<Fs...> FunctionOverload(Fs...
 	return {fs...};
 }
 
-
 // OverloadCheck(can_call, &ptr, x->Method(x))
 // OverloadCheck(can_cast, &ptr, (type)x)
 // if constexpr(can_cast && can_call) { ... }
@@ -480,7 +353,6 @@ template<class...Fs> constexpr FunctionOverloadSet<Fs...> FunctionOverload(Fs...
 		[](auto&& x) -> decltype(expr, 1U) {return{};}, \
 		[](...)      -> char { return {}; } \
 	);constexpr auto result = sizeof(decltype(_gL_##result(pointer))) == 4;
-
 
 // all supported format specifiers
 enum Formats {
