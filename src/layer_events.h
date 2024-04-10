b5ee2b1b4ee3e72fa7883e8b9d1cd013d9e132e7
mittorn <mittorn@sibmail.com>
2024-04-11 02:04:31 +0300

    Implement network sending, send session data

--- src/layer_events.h
+++ src/layer_events.h
@@ -3,13 +3,10 @@
 #include "layer_utl.h"
 #include "thread_utl.h"
 
-enum TargetFlags : unsigned char
-{
-	TARGET_APP = 1U << 0,
-	TARGET_BUS = 1U << 1,
-	TARGET_CLI = 1U << 2,
-	TARGET_GUI = 1U << 3
-};
+#define TARGET_APP (1U << 0)
+#define TARGET_BUS (1U << 1)
+#define TARGET_CLI (1U << 2)
+#define TARGET_GUI (1U << 3)
 
 enum EventType : unsigned char
 {
@@ -23,6 +20,7 @@ enum EventType : unsigned char
 	EVENT_APP_CUSTOM_ACTION,
 	EVENT_APP_BINDING,
 	EVENT_APP_RPN,
+	EVENT_APP_SESSION,
 	EVENT_DIAGMSG,
 	EVENT_COMMAND
 };
@@ -45,7 +43,7 @@ enum CommandType{
 
 struct CommandHeader
 {
-	CommandType type;
+	CommandType ctype;
 	union{
 		unsigned int i;
 		float f;
@@ -56,8 +54,9 @@ struct CommandHeader
 
 struct Command : CommandHeader
 {
+	constexpr static EventType type = EVENT_COMMAND;
 	char data[MAX_CMDLEN - sizeof(CommandHeader)];
-	Command(){}
+	Command() = default;
 	void _AddStr(const SubStr &s, int arg)
 	{
 		int len = s.Len();
@@ -72,19 +71,19 @@ struct Command : CommandHeader
 	Command(const SubStr &s)
 	{
 		datasize = 0;
-		type = EVENT_POLL_NULL;
+		ctype = EVENT_POLL_NULL;
 		SubStr s1, s2; // todo: encode signature to string/array instead
 		if(!s.Split2(s1, s2, ' '))
 		s1 = s, s2 = "";
 		{
 			if(s1.Equals("triggerInteractionProfileChanged"))
-				type = EVENT_POLL_TRIGGER_INTERACTION_PROFILE_CHANGED;
+				ctype = EVENT_POLL_TRIGGER_INTERACTION_PROFILE_CHANGED;
 			else if(s1.Equals("reloadConfig"))
-				type = EVENT_POLL_RELOAD_CONFIG;
+				ctype = EVENT_POLL_RELOAD_CONFIG;
 			else if(s1.Equals("dumpAppBindings"))
-				type = EVENT_POLL_DUMP_APP_BINDINGS;
+				ctype = EVENT_POLL_DUMP_APP_BINDINGS;
 			else if(s1.Equals("dumpLayerBindings"))
-				type = EVENT_POLL_DUMP_LAYER_BINDINGS;
+				ctype = EVENT_POLL_DUMP_LAYER_BINDINGS;
 			else
 			{
 				if(!s2.Len())
@@ -97,7 +96,7 @@ struct Command : CommandHeader
 						_AddStr(s3, 0);
 						args[1].i = atoi(s5.begin);
 						args[2].f = atof(s6.begin);
-						type = EVENT_POLL_SET_EXTERNAL_SOURCE;
+						ctype = EVENT_POLL_SET_EXTERNAL_SOURCE;
 					}
 				}
 				else if(s1.Equals("mapDirectSource"))
@@ -108,7 +107,7 @@ struct Command : CommandHeader
 						_AddStr(s3, 0);
 						args[1].i = atoi(s5.begin);
 						_AddStr(s6, 2);
-						type = EVENT_POLL_MAP_DIRECT_SOURCE;
+						ctype = EVENT_POLL_MAP_DIRECT_SOURCE;
 					}
 				}
 				else if(s1.Equals("mapAxis"))
@@ -120,17 +119,17 @@ struct Command : CommandHeader
 						args[1].i = atoi(s5.begin);
 						_AddStr(s7, 2);
 						args[3].i = atoi(s8.begin);
-						type = EVENT_POLL_MAP_AXIS;
+						ctype = EVENT_POLL_MAP_AXIS;
 					}
 				}
 				else if(s1.Equals("resetAction"))
 				{
-					type = EVENT_POLL_RESET_ACTION;
+					ctype = EVENT_POLL_RESET_ACTION;
 					_AddStr(s2, 0);
 				}
 				else if(s1.Equals("setProfile"))
 				{
-					type = EVENT_POLL_SET_PROFILE;
+					ctype = EVENT_POLL_SET_PROFILE;
 					_AddStr(s2, 0);
 				}
 				else if(s1.Equals("mapAction"))
@@ -142,7 +141,7 @@ struct Command : CommandHeader
 						_AddStr(s3, 0);
 						_AddStr(s5, 1);
 						args[2].i = atoi(s6.begin);
-						type = EVENT_POLL_MAP_ACTION;
+						ctype = EVENT_POLL_MAP_ACTION;
 					}
 				}
 			}
@@ -150,7 +149,7 @@ struct Command : CommandHeader
 	}
 	Command & operator = (const Command &other)
 	{
-		type = other.type;
+		ctype = other.ctype;
 		memcpy(args, other.args, sizeof(other.args));
 		datasize = other.datasize;
 		memcpy(data, other.data, datasize);
@@ -177,12 +176,15 @@ struct Field_
 	}
 	Field_(const Field_ &other) = delete;
 	Field_& operator=(const Field_ &) = delete;
-	Field_(){}
+	Field_() = default;
 	Field_(const T &def):val(def){}
 	template <typename DumperType>
 	Field_(Dumper<DumperType> &l){
 		char s[32];
-		SBPrint(s, "%f", (float)l.GetData(this).val);
+		if constexpr(sizeof(T) == 8 && ((T)0.1 == (T)0.0))
+			SBPrint(s, "%lld", (unsigned long long)l.GetData(this).val);
+		else
+			SBPrint(s, "%f", (double)l.GetData(this).val);
 		l.d.Dump(SubStr(NAME,nlen), SubStrB(s));
 	}
 };
@@ -198,7 +200,7 @@ struct StringField_
 	char val[len];
 	StringField_(const StringField_ &other) = delete;
 	StringField_& operator=(const StringField_ &) = delete;
-	StringField_(){}
+	StringField_() = default;
 	StringField_(const SubStr &def):val(){ def.CopyTo(val);}
 	template <typename DumperType>
 	StringField_(Dumper<DumperType> &l){
@@ -206,128 +208,125 @@ struct StringField_
 	}
 };
 
+struct AppReg
+{
+	constexpr static EventType type = EVENT_APP_REGISTER;
+	StringField(name,32);
+	Field(unsigned int, version);
+	StringField(engine,32);
+};
+
+struct AppSession
+{
+	constexpr static EventType type = EVENT_APP_SESSION;
+	Field(unsigned long long, handle);
+	Field(unsigned int, state);
+};
+
+struct AppVar
+{
+	constexpr static EventType type = EVENT_APP_VAR;
+	StringField(name,32);
+	Field(float,value);
+};
+struct AppAction
+{
+	constexpr static EventType type = EVENT_APP_ACTION;
+	Field(unsigned long long,handle);
+	Field(unsigned long long,session);
+	StringField(setName,32);
+	StringField(actName,32);
+};
+
+struct AppActionSet
+{
+	constexpr static EventType type = EVENT_APP_ACTION_SET;
+	Field(unsigned long long,handle);
+	Field(unsigned long long,session);
+	StringField(setName,32);
+	StringField(description,32);
+};
+struct AppBinding
+{
+	constexpr static EventType type = EVENT_APP_BINDING;
+	Field(unsigned long long,handle);
+	StringField(path,32);
+	StringField(description,32);
+};
+struct AppSource
+{
+	constexpr static EventType type = EVENT_APP_SOURCE;
+	Field(unsigned long long,session);
+	StringField(name,32);
+	Field(int, index);
+	Field(float,x);
+	Field(float,y);
+};
+struct AppRPN
+{
+	constexpr static EventType type = EVENT_APP_RPN;
+	Field(unsigned long long,session);
+	StringField(source,64);
+	Field(unsigned char,context);
+	Field(int,index);
+	StringField(rpn,64);
+};
+struct AppActionMap
+{
+	constexpr static EventType type = EVENT_APP_ACTION_MAP;
+	Field(unsigned long long,session);
+	StringField(actName,32);
+	Field(int,mapIndex);
+	Field(int,funcIndex);
+	Field(int,actionIndex);
+	Field(int,axisIndex);
+	Field(int,handIndex);
+};
+
+struct AppCustomAction
+{
+	constexpr static EventType type = EVENT_APP_CUSTOM_ACTION;
+	Field(unsigned long long,session);
+	Field(int, index);
+	StringField(command, 64);
+	StringField(condition, 64);
+	Field(float, triggerPeriod);
+};
+
+struct DiagMsg
+{
+	constexpr static EventType type = EVENT_DIAGMSG;
+	Field(unsigned int,level);
+	Field(unsigned int,code);
+	StringField(message, 64);
+};
+
 struct EventHeader
 {
-	TargetFlags target;
+	unsigned char target;
 	EventType type;
 	char displayName[14];
 	pid_t targetPid, sourcePid;
+};
+
+struct EventPacket
+{
+	EventHeader head;
 	union EventData
 	{
 		Command cmd;
-		struct AppRegister
-		{
-			Field(pid_t,pid);
-		} appReg;
-		struct AppVar
-		{
-			StringField(name,32);
-			Field(float,value);
-		} appVar;
-		struct AppAction
-		{
-			Field(unsigned long long,handle);
-			StringField(setName,32);
-			StringField(actName,32);
-		} appAct;
-		struct AppBinding
-		{
-			Field(unsigned long long,handle);
-			StringField(path,32);
-			StringField(description,32);
-		} appBinding;
-		struct AppSource
-		{
-			StringField(name,32);
-			Field(int, index);
-			Field(float,x);
-			Field(float,y);
-		} appSource;
-		struct AppRPN
-		{
-			StringField(source,64);
-			Field(unsigned char,context);
-			Field(int,index);
-			StringField(rpn,64);
-		} appRPN;
-		struct AppActionMap
-		{
-			StringField(actName,32);
-			Field(int,mapIndex);
-			Field(int,funcIndex);
-			Field(int,actionIndex);
-			Field(int,axisIndex);
-			Field(int,handIndex);
-		} appActionMap;
-
-		struct AppCustomAction
-		{
-			Field(int, index);
-			StringField(command, 64);
-			StringField(condition, 64);
-			Field(float, triggerPeriod);
-		} appCustomAction;
-
-		struct DiagMsg
-		{
-			Field(unsigned int,level);
-			Field(unsigned int,code);
-			StringField(message, 64);
-		} diagMsg;
+		AppReg appReg;
+		AppVar appVar;
+		AppSession appSession;
+		AppAction appAction;
+		AppActionSet appActionSet;
+		AppBinding appBinding;
+		AppSource appSource;
+		AppRPN appRPN;
+		AppActionMap appActionMap;
+		AppCustomAction appCustomAction;
+		DiagMsg diagMsg;
 	} data;
-};
-
-struct EventPoller
-{
-	Thread pollerThread;
-	EventPoller() : pollerThread([](void *poller){
-			EventPoller *p = (EventPoller*)poller;
-			p->Run();
-		},this) {}
-	CycleQueue<Command> pollEvents;
-	SpinLock pollLock;
-	int fd = -1;
-	bool InitSocket()
-	{
-		fd = open("/tmp/command_pipe", O_RDONLY);
-		return fd >= 0;
-	}
-	void Run()
-	{
-		char buf[256];
-		int pos = 0;
-		while( read(fd, &buf[pos], 1) > 0 )
-		{
-			if(!Running)
-				return;
-			if(pos >= 255)
-				break;
-			if(buf[pos] == '\n')
-			{
-				buf[pos] = 0;
-				SubStr s = SubStr(buf, pos);
-				pos = 0;
-				Command c = Command(s);
-				pollEvents.Enqueue(c);
-			}
-			else pos++;
-		}
-	}
-	volatile bool Running = false;
-	void Start(int port)
-	{
-		if(!InitSocket())
-			return;
-		Running = true;
-		SyncBarrier();
-		pollerThread.Start();
-	}
-	void Stop()
-	{
-		Running = false;
-		SyncBarrier();
-		pollerThread.RequestStop();
-	}
 };
 
 #endif // LAYER_EVENTS_H
